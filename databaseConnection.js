1ba063cf7ebcfb5dac4e11c631f3234245286d90
BCITKevin <kso51@my.bcit.ca>
2024-02-16 00:13:56 -0800

    editing dbconfig

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,24 +1,24 @@
 const mysql = require('mysql2/promise');
 
-const is_qoddi = process.env.IS_QODDI || false;
+const is_render = process.env.IS_RENDER || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
+	host: "sql.freedb.tech",
+	user: "freedb_2350_main..",
+	password: "svW5VCbNE?gY*q3",
+	database: "freedb_comp2350-week5-A01296033",
 	multipleStatements: false
 };
 
 const dbConfigLocal = {
 	host: "localhost",
 	user: "root",
-	password: "Password",
+	password: "kevin02gusgh02",
 	database: "database1",
 	multipleStatements: false
 };
 
-if (is_qoddi) {
+if (is_render) {
 	var database = mysql.createPool(dbConfigQoddi);
 }
 else {
